ef69dbd78a3800e70d6f2f15dac0b067c1b0b803
Saboor Ahmed Khan <saboorahmedkhan@gmail.com>
2023-05-10 14:46:46 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Card from '../../component/card';
 export default function Home() {
   return (
     <div>
-      <h3>Hello Saboor</h3>
+      <h3>Hello World</h3>
       <Card />
       <Card />
       <Card />
