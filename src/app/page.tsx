fe3ac31c350138e300b54c135a125ecbfb64857d
Saboor Ahmed Khan <saboorahmedkhan@gmail.com>
2023-05-10 15:14:52 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Card from '../../component/card';
 export default function Home() {
   return (
     <div>
-      <h3>Hello World PAKBIT</h3>
+      <h3>Hello PAKBIT</h3>
       <Card />
       <Card />
       <Card />
